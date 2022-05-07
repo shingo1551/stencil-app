49308cd3e8e52e6193bd8af432138932a1a2e3f2
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-05-07 11:48:34 +0900

    onClick

--- src/shared/menu.tsx
+++ src/shared/menu.tsx
@@ -1,12 +1,9 @@
-import { FunctionalComponent, h } from '@stencil/core';
+import { h } from '@stencil/core';
 import { href as _href } from '@stencil/router';
 
-export const A = (props, children) => <li><a {..._href(props.href)} onClick={props.onClick}>{children}</a></li>;
-
-interface Props {
-  href: string;
-}
-
-export const A2: FunctionalComponent<Props> = ({ href }, children) => (
-  <li><a {..._href(href)}>{children}</a></li>
-);
+export const A = (props, children) =>
+  <li>
+    {props.onClick
+      ? <a {..._href(props.href)} onClick={props.onClick}>{children}</a>
+      : <a {..._href(props.href)} >{children}</a>}
+  </li>;

f7b96300ef8eb45bc4b6e4209cb72a29f6ee3ab9
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-05-07 14:32:21 +0900

    FunctionalComponent

--- src/shared/menu.tsx
+++ src/shared/menu.tsx
@@ -1,9 +1,16 @@
-import { h } from '@stencil/core';
-import { href as _href } from '@stencil/router';
+import { h, FunctionalComponent } from '@stencil/core';
+import { href } from '@stencil/router';
 
-export const A = (props, children) =>
+// export const A = (props, children) =>
+//   <li>
+//     {props.onClick
+//       ? <a {...href(props.href)} onClick={props.onClick}>{children}</a>
+//       : <a {...href(props.href)} >{children}</a>}
+//   </li>;
+
+export const A: FunctionalComponent<{ href: string, onClick?: (e: MouseEvent) => void }> = (props, children) =>
   <li>
     {props.onClick
-      ? <a {..._href(props.href)} onClick={props.onClick}>{children}</a>
-      : <a {..._href(props.href)} >{children}</a>}
+      ? <a {...href(props.href)} onClick={props.onClick}>{children}</a>
+      : <a {...href(props.href)} >{children}</a>}
   </li>;
